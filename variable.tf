ed8f48f51873fe428c00fcf2079d029bef45cddf
v2hakak <82645704+v2hakak@users.noreply.github.com>
2021-04-20 14:09:29 +0530

    terraform script

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable ami {
+default = "ami-01e7ca2ef94a0ae86"
+}
+
+variable region {
+    default = "us-east-2"
+}
+
+variable project {
+    default = "jenkins"
+}
+variable mysourcesg {
+    default = "0.0.0.0/0"
+}
